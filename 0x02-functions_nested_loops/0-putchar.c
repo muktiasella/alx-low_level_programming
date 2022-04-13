f765aec40b8003a2c87be6999039f6cb26b7ee6b
muktiasella <mohammedmuktar685827@gmail.com>
2022-04-13 08:39:03 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main."
+
+/**
+ * main -prints _putchar
+ *
+ * description:prints _putchar
+ *
+ *return: 0 (Success)
+ */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0 i < 8; i++)
+	{
+		_putchar(text[0]);
+	}
+	_putchar  ('\n');
+
+	return (0);
+}
